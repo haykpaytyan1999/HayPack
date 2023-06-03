1276b02ace5bb333c0e84371071ef3f2431b5dc4
haykpaytyan1999 <haykpaytyan0@gmail.com>
2023-06-03 01:38:21 -0700

    add videos

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "../component/header/Header";
 import Carousel from "../component/carousel/Carousel";
